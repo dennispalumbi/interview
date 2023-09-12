da71abb2efff38f27c0fc4666270c2073b422ffb
dennispalumbi <dennispalumbi@gmail.com>
2023-09-12 05:17:18 -0400

    Initial commit

---
+++ src/main/java/com/houston/interview/Interview.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.houston.interview;
+import java.util.Scanner;
+
+/**
+ *
+ * @author dens
+ */
+public class Interview {
+
+    public static void main(String[] args) {
+        int value1=scValues(1);
+        int value2=scValues(2);
+        int value3=scValues(3);
+        String result="none";
+        if(value1+value2==value3)
+            result="+";
+        if(value1-value2==value3)
+            result=result+"-";
+        if(value1*value2==value3)
+            result=result+"*";
+        if(value1/value2==value3)
+            result=result+"/";
+        
+        
+        System.out.println("kind operation result:"+result);  // Output user input
+    }
+    public static int scValues(int nparam){
+        //Scanning Param
+        Scanner v1 = new Scanner(System.in);  
+        System.out.println("Enter param "+nparam);
+        //Converting to INT Param 
+        return  Integer.parseInt(v1.nextLine());
+    }
+    
+    
+}
